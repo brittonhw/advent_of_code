b723c49a70427b935a585387359266698443186e
Henry Britton <76451813+brittonhw@users.noreply.github.com>
2025-12-10 14:02:50 -0500

    part 2 of day 2

--- 2025/day2.py
+++ 2025/day2.py
@@ -10,13 +10,15 @@ Find, then add up together all of the invalid ids.
 
 '''
 from ingest import read_file
+import textwrap
 
-
+# break into ranges
 def parse_as_range(input_item):
     item_range = input_item.split('-')
     print( 'Length of range:', int(item_range[1]) - int(item_range[0]) + 1)
     return range(int(item_range[0]), int(item_range[1]) + 1)
 
+# break and half and check if both halves are the same
 def find_single_repeating_ids(input_list):
     total = 0
     for item in input_list:
@@ -25,18 +27,35 @@ def find_single_repeating_ids(input_list):
             length = len(stringified_id)
             half_length = length // 2
             if stringified_id[:half_length] == stringified_id[half_length:]:
-                #print(stringified_id)
                 total += id
     return total
 
+# break into chunks of n and check if all are the same
+def find_n_repeating_ids(input_list):
+    total = 0
+    for item in input_list:
+        for id in parse_as_range(item):
+            stringified_id = str(id)
+            length = len(stringified_id)
+            # only need to check up to half the length
+            for i in range(1, length//2 + 1):
+                wrapped = textwrap.wrap(stringified_id, i)
+                if wrapped and all(x == wrapped[0] for x in wrapped):
+                    total += id
+                    # once found, no need to check other lengths, and don't want to multi-count
+                    break
+    return total
 
 def main():
     input = read_file('day2/input.txt')
     input = input[0].split(',')
 
-    answer = find_single_repeating_ids(input)
+    p1_answer = find_single_repeating_ids(input)
+    p2_answer = find_n_repeating_ids(input)
+
     
-    print("Final Total:", answer)
+    print("P1 Total:", p1_answer)
+    print("P2 Total:", p2_answer)
 
 
 
