9bdf3796e51a60693120a8348cddb4c67476784e
Henry Britton <76451813+brittonhw@users.noreply.github.com>
2025-12-10 14:06:54 -0500

    init day3

---
+++ 2025/day3.py
@@ -0,0 +1,18 @@
+'''
+Solution file for Advent of Code Day 3
+https://adventofcode.com/2025/day/3
+
+Puzzle summary:
+
+
+'''
+from ingest import read_file
+
+
+def main():
+    input = read_file('day3/input.txt')
+    # implement solution here
+
+
+if __name__ == '__main__':
+    main()
