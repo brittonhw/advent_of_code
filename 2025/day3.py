d078c20166f82e1c893f40dfedc165c3f602b550
Henry Britton <76451813+brittonhw@users.noreply.github.com>
2025-12-10 15:27:36 -0500

    day3 pt 2

--- 2025/day3.py
+++ 2025/day3.py
@@ -8,11 +8,11 @@ Puzzle summary:
 '''
 from ingest import read_file
 
-def bank_max_joltage(bank):
-    bank_i = [int(bank[i]) for i in range(len(bank))]
+# naive way for pt1
+def bank_max_joltage(bank_i):
     biggest_digit = max(bank_i)
     index_max = bank_i.index(biggest_digit)
-    if index_max == len(bank) - 1:
+    if index_max == len(bank_i) - 1:
         second_digit = biggest_digit
         first_digit = max(bank_i[:-1])
     else:
@@ -23,20 +23,40 @@ def bank_max_joltage(bank):
     return (10 * first_digit) + second_digit
 
 
+# greedy baby: 12 biggest joltage: biggest number 12 or more end, then biggest number 11 or more before rest, etc.
+def n_total_joltage(bank, n):
+    if n == 0:
+        return max(bank)
+    eligible_range = bank[:-n]
+    max_digit = max(eligible_range)
+    digit_index = bank.index(max_digit)
+    return (max_digit * (10**n)) + n_total_joltage(bank[digit_index+1:], n - 1 )
+
+
 
 
 def total_joltage(bank_list):
     total = 0
+    total_12bats = 0
     for bank in bank_list:
-        total += bank_max_joltage(bank)
+        bank_i = [int(bank[i]) for i in range(len(bank))]
+        total += bank_max_joltage(bank_i)
+        total_12bats += n_total_joltage(bank_i, 11)
     
-    return total
+    return total, total_12bats
 
 def main():
     input = read_file('day3/input.txt')
+    print(input[0])
     
-    pt1_answer = total_joltage(input)
+    pt1_answer, pt2_answer = total_joltage(input)
+
     print('Pt1 Answer:', pt1_answer)
+    print('Pt2 Answer:', pt2_answer)
+
+
+
+
 
 
 
