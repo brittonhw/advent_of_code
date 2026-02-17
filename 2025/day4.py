6f4e21638b0c22eaa400d15cdf360d560b221d17
Henry Britton <76451813+brittonhw@users.noreply.github.com>
2026-02-17 13:46:43 -0500

    kidn of some nonsense i dont remember making

--- 2025/day4.py
+++ 2025/day4.py
@@ -1,4 +1,5 @@
 from ingest import read_file
+import copy
 
 def is_roll(input, row, col):
     row_dim = len(input)
@@ -8,7 +9,7 @@ def is_roll(input, row, col):
     else:
         return input[row][col] == '@'
 
-def tally_neighbors(input):
+def find_accessible_rolls(input):
 
     grid = [list(line) for line in input]
 
@@ -32,14 +33,58 @@ def tally_neighbors(input):
 
     return accessible_rolls
 
+def check_removables(neighbor_map, input):
+    print(neighbor_map)
+    for row in range(len(neighbor_map)):
+        for col in range(len(neighbor_map[0])):
+            if neighbor_map[row][col] < 4 and is_roll(input, row, col):
+                return True
+    
+    return False
 
 
+def find_accessible_rolls_with_mutation(input):
+    
+    input_copy = []
+    for line in input:
+        added_line = []
+        for item in list(line):
+            added_line.append(0) if item == '.' else added_line.append(1)
+        input_copy.append(added_line)
+
+
+    neighbors_map = [[0 for col in row] for row in input]
+
+    accessible_rolls = 0
+    
+    while check_removables(neighbors_map, input):
+
+        for row in range(len(input_copy)):
+            for col in range(len(input_copy[0])):
+                if is_roll(input_copy, row, col):
+                    neighbors_map[row][col] += 1 if is_roll(input_copy, row + 1, col) else 0
+                    neighbors_map[row][col] += 1 if is_roll(input_copy, row + 1, col + 1) else 0
+                    neighbors_map[row][col] += 1 if is_roll(input_copy, row + 1, col - 1) else 0
+                    neighbors_map[row][col] += 1 if is_roll(input_copy, row, col + 1) else 0
+                    neighbors_map[row][col] += 1 if is_roll(input_copy, row, col - 1) else 0
+                    neighbors_map[row][col] += 1 if is_roll(input_copy, row - 1, col) else 0
+                    neighbors_map[row][col] += 1 if is_roll(input_copy, row - 1, col + 1) else 0
+                    neighbors_map[row][col] += 1 if is_roll(input_copy, row - 1, col - 1) else 0
+
+                    if neighbors_map[row][col] < 4:
+                        accessible_rolls += 1
+                        input_copy[row][col] = '.'
+
+    return accessible_rolls
+    
 
 
 def main():
     input = read_file('day4/input.txt')
     
-    print(tally_neighbors(input))
+    print(find_accessible_rolls(input))
+
+    print(find_accessible_rolls_with_mutation(input.copy()))
     
     
 
